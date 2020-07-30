db1ec5e12a063976fbc7907f3e5abfe09401af8b
alphadatagamma <alphadatagamma@users.noreply.github.com>
2020-07-29 23:29:05 -0400

    Firts commit

---
+++ app.py
@@ -0,0 +1,20 @@
+### Main page for streamlit resume
+import streamlit as st
+
+st.sidebar.title("Hire Me")
+st.sidebar.info(
+        """
+        If you are looking to hire a Data Scientist, 
+        [email me](mailto:abhishek.2.gupta@uconn.edu) or reach out 
+        to me on [LinkedIn](https://www.linkedin.com/in/abhishek-gupta-/)
+""")
+st.sidebar.title("Additional Info")
+st.sidebar.info(
+        "This an interactive streamlit app completely created with Python's latest library **streamlit** "
+        "Do reach out to me on [LinkedIn](https://www.linkedin.com/in/abhishek-gupta-/) or "
+        "at [Mail me](mailto:abhishek.2.gupta@uconn.edu) to know more. "
+        "Also check the [source code](https://github.com/alphadatagamma/Streamlit-Resume-App) here. "  
+
+)
+
+
